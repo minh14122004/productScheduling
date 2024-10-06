4f8b78168a7b0f1371c175cb39ec663576f7a985
minh1412204 <quangminh14120805@gmail.com>
2024-10-06 18:44:34 +0700

    set Role Feature

--- ABC Production Scheduling System/src/java/entity/Feature.java
+++ ABC Production Scheduling System/src/java/entity/Feature.java
@@ -4,10 +4,54 @@
  */
 package entity;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Nguyễn Quang Minh
  */
 public class Feature {
+    public int fID;
+    public String featureName;
+    public ArrayList<Role> rID;
+
+    public Feature() {
+    }
+
+    public Feature(int fID, String featureName, ArrayList<Role> rID) {
+        this.fID = fID;
+        this.featureName = featureName;
+        this.rID = rID;
+    }
+
+    public int getfID() {
+        return fID;
+    }
+
+    public void setfID(int fID) {
+        this.fID = fID;
+    }
+
+    public String getFeatureName() {
+        return featureName;
+    }
+
+    public void setFeatureName(String featureName) {
+        this.featureName = featureName;
+    }
+
+    public ArrayList<Role> getrID() {
+        return rID;
+    }
+
+    public void setrID(ArrayList<Role> rID) {
+        this.rID = rID;
+    }
+
+    @Override
+    public String toString() {
+        return "Feature{" + "fID=" + fID + ", featureName=" + featureName + ", rID=" + rID + '}';
+    }
+    
     
 }
--- ABC Production Scheduling System/src/java/entity/Role.java
+++ ABC Production Scheduling System/src/java/entity/Role.java
@@ -4,10 +4,54 @@
  */
 package entity;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Nguyễn Quang Minh
  */
 public class Role {
+    public int rID;
+    public String nameRole;
+    public ArrayList<Feature> fID;
+
+    public Role() {
+    }
+
+    public Role(int rID, String nameRole, ArrayList<Feature> fID) {
+        this.rID = rID;
+        this.nameRole = nameRole;
+        this.fID = fID;
+    }
+
+    public int getrID() {
+        return rID;
+    }
+
+    public void setrID(int rID) {
+        this.rID = rID;
+    }
+
+    public String getNameRole() {
+        return nameRole;
+    }
+
+    public void setNameRole(String nameRole) {
+        this.nameRole = nameRole;
+    }
+
+    public ArrayList<Feature> getfID() {
+        return fID;
+    }
+
+    public void setfID(ArrayList<Feature> fID) {
+        this.fID = fID;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" + "rID=" + rID + ", nameRole=" + nameRole + ", fID=" + fID + '}';
+    }
+    
     
 }
