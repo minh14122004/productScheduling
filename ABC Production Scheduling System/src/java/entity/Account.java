4ae404b2df1c06f7d1a8adb387204a1ed6547c7d
minh1412204 <quangminh14120805@gmail.com>
2024-10-06 17:52:31 +0700

    des Account

--- ABC Production Scheduling System/src/java/entity/Account.java
+++ ABC Production Scheduling System/src/java/entity/Account.java
@@ -9,5 +9,58 @@ package entity;
  * @author Nguyễn Quang Minh
  */
 public class Account {
+    public int aID;
+    public int rID;
+    private String username;
+    private String password;
+
+    public Account() {
+    }
+
+    public Account(int aID, int rID, String username, String password) {
+        this.aID = aID;
+        this.rID = rID;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getaID() {
+        return aID;
+    }
+
+    public void setaID(int aID) {
+        this.aID = aID;
+    }
+
+    public int getrID() {
+        return rID;
+    }
+
+    public void setrID(int rID) {
+        this.rID = rID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "aID=" + aID + ", rID=" + rID + ", username=" + username + ", password=" + password + '}';
+    }
+
     
+
 }
