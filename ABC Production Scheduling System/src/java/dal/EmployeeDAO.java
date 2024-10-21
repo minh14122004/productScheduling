13b2eccced37c8deebc46ec381c1c6767d518caf
minh1412204 <quangminh14120805@gmail.com>
2024-10-21 09:31:18 +0700

    em DAO

--- ABC Production Scheduling System/src/java/dal/EmployeeDAO.java
+++ ABC Production Scheduling System/src/java/dal/EmployeeDAO.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  *
  * @author Nguyễn Quang Minh
  */
-public class EmployeeDAO extends DBContext<Employee>{
+public class EmployeeDAO extends DBContext<Employee> {
 
     @Override
     public void insert(Employee entity) {
@@ -33,9 +33,35 @@ public class EmployeeDAO extends DBContext<Employee>{
 
     @Override
     public ArrayList<Employee> list() {
-        String sql ="SELECT * FROM Employee;";
+        String sql = "SELECT * FROM Employee;";
         PreparedStatement stm = null;
-        Employee employ = null;
+
+        try {
+            stm= connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                Employee e = new Employee();
+                e.setrID(rs.getInt("EmployeeID"));
+                e.seteName(rs.getString("EmployeeName"));
+                e.setGender(rs.getBoolean("gender"));
+                e.setAddress(rs.getString("address"));
+                e.setDob(rs.getDate("dob"));
+                e.setrID(rs.getInt("RoleID"));
+                e.setdID(rs.getInt("DepartmentID"));
+                e.setSalary(rs.getLong("salary"));
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                stm.close();
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
         return null;
     }
 
@@ -43,5 +69,5 @@ public class EmployeeDAO extends DBContext<Employee>{
     public Employee get(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
