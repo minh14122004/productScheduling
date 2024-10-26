b17fd29b3d0fa6150a0ad913b46abc9934a219c7
minh1412204 <quangminh14120805@gmail.com>
2024-10-26 16:52:25 +0700

    View employee

---
+++ ABC Production Scheduling System/src/java/controller/employee/EmployeeViewController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.employee;
+
+import controller.accesscontrol.BaseRBACController;
+import dal.EmployeeDAO;
+import entity.Employee;
+import entity.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nguyễn Quang Minh
+ */
+public class EmployeeViewController extends BaseRBACController {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        EmployeeDAO db = new EmployeeDAO();
+        ArrayList<Employee> emps = db.list();
+        request.setAttribute("emps", emps);
+        request.getRequestDispatcher("../view/employee/list.jsp").forward(request, response);
+        
+    } 
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    @Override
+    protected void doAuthorizedGet(HttpServletRequest req, HttpServletResponse resp, User account) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doAuthorizedPost(HttpServletRequest req, HttpServletResponse resp, User account) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+}
